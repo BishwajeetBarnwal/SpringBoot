88560dcc125b04e1eba166a765840ab182c26a77
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-09-28 13:12:03 +0530

    First Spring Boot Application Committed

---
+++ restful-web-services/src/main/java/com/springbootapplication/restfulwebservices/helloworld/HelloWorldCOntroller.java
@@ -0,0 +1,18 @@
+package com.springbootapplication.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldCOntroller {
+
+	
+	//@RequestMapping(method = RequestMethod.GET,  path = "/hello-world")
+	@GetMapping( path = "/hello-world")
+	
+	public String helloWorld() {
+		return "Hello World" ;
+	}
+}
