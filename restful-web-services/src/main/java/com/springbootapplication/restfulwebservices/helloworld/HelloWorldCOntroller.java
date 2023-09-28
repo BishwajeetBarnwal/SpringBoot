36f336bb852a3f331ca6094f24de2e699b95c526
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-09-28 15:32:43 +0530

    Update HelloWorldCOntroller.java

--- restful-web-services/src/main/java/com/springbootapplication/restfulwebservices/helloworld/HelloWorldCOntroller.java
+++ restful-web-services/src/main/java/com/springbootapplication/restfulwebservices/helloworld/HelloWorldCOntroller.java
@@ -1,6 +1,7 @@
 package com.springbootapplication.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,11 @@ public class HelloWorldCOntroller {
 		
 		public HelloWorldBean helloWorldBean() {
 			return new HelloWorldBean("Hello World") ;
+	}
+
+	@GetMapping( path = "/hello-world/path-variable/{name}")
+		
+		public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+			return new HelloWorldBean(String.format("Hello World,  %s" + name) );
 	}
 }
