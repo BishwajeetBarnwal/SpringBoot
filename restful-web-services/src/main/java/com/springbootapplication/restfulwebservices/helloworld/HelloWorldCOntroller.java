ef5960b1252cd5e0bca9f00b1ee9a7d50f37cc1f
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-09-28 15:14:06 +0530

    Update HelloWorldCOntroller.java

--- restful-web-services/src/main/java/com/springbootapplication/restfulwebservices/helloworld/HelloWorldCOntroller.java
+++ restful-web-services/src/main/java/com/springbootapplication/restfulwebservices/helloworld/HelloWorldCOntroller.java
@@ -14,5 +14,11 @@ public class HelloWorldCOntroller {
 	
 	public String helloWorld() {
 		return "Hello World" ;
+		
+	}
+		@GetMapping( path = "/hello-world-bean")
+		
+		public HelloWorldBean helloWorldBean() {
+			return new HelloWorldBean("Hello World") ;
 	}
 }
